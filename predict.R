755f2177328f99a9d01a2bf0b5a845e3aa21602e
William <rf65@duke.edu>
2014-11-25 16:04:49 -0800

    Predicting Methylation Heterogeneity Regions (MHRs).

--- predict.R
+++ predict.R
@@ -16,7 +16,7 @@ ref <- matchPattern("CG", genome.sub, max.mismatch=0)
 ref <- GRanges(seqnames=sel.chr, IRanges(start(ref), width=1))
 
 # load in the reads from 3000000 to 3500000 on chr1 plus strand as an example
-bam.name <- "/data/illumina_runs/data10/r3fang/allc_filtered/allc_h1_db/h1_processed_reads_no_clonal.bam"
+bam.name <- "/data/home/yupeng/test_BSEQC/h1_processed_reads_no_clonal.bam"
 
 MHR.gr <- predictMHR("chr1", 300000, 1000000000, chunkSize=1000000, nCG=6, min.Cover=10, step=3, num.cores=20, bam.name)
 
@@ -25,8 +25,8 @@ sel.start = min(start(ref));
 sel.end = max(start(ref));
 chunkSize=10000000;
 #chunkSize=1000000;
-nCG = 4; # number of CpG in each segments
-min.Cover = 8; # min coverage to be counteds
+nCG = 6; # number of CpG in each segments
+min.Cover = 10; # min coverage to be counteds
 step = 2; # window moves 2 CpG each time
 num.cores = 20
 
@@ -34,20 +34,58 @@ num.cores = 20
 
 MHRs.gr <- GRanges(seqlengths=seqlengths(genome)[1:24])
 for(sel.start in seq(min(start(ref)) - 500, max(end(ref)) + chunkSize, by=chunkSize)){
-	print(sel.start)
-	#which <- GRanges(sel.chr, IRanges(sel.start, sel.start + chunkSize))
-	which <- GRanges("chr6", IRanges(min(start(ref)), max(start(ref))))
+	
+	# Step I: loading all reads within given  chunk regions.
+	which <- GRanges(sel.chr, IRanges(sel.start, sel.start + chunkSize))
 	what <- c("seq")
 	flag <- scanBamFlag(isMinusStrand = FALSE)
 	param <- ScanBamParam(which=which, what=what, flag=flag)
 	gals <- readGAlignments(bam.name, param=param)
-	# identify regions with coverage greater than min.Cover
+	if(length(gals)==0){next}
+
+	# Step II: filter genome regions with coverage smaller than min coverage
 	peaks <- slice(coverage(gals)[[sel.chr]], lower=min.Cover)
 	peaks.gr <- GRanges(seqnames=sel.chr, IRanges(start(peaks), end(peaks)))
-	# filter peaks containing CpG less than nCG and split peaks to possible segments
+	
+	# Step III: filter high-coverage regions but contain CpG smaller than nCG.
 	ov <- findOverlaps(ref, peaks.gr)
-	sel.ind.CG <- Filter(function(x){length(x)>=nCG}, split(ov@queryHits, ov@subjectHits))
-	peaks.filter.gr <- peaks.gr[as.integer(names(sel.ind.CG))]
+	peak.CG.ind <- Filter(function(x){length(x)>=nCG}, split(ov@queryHits, ov@subjectHits))
+	peaks.filter.gr <- peaks.gr[as.integer(names(peak.CG.ind))]
+	
+	# Step IV: find all the segments in the high-coverage regions.
+	res <- mclapply(peak.CG.ind, function(x){
+		res <- lapply(seq(1, (length(x)-nCG+1)), function(i){
+			ind.tmp <- x[i:(i+nCG-1)]	
+			gr.tmp <- GRanges(seqnames=sel.chr, IRanges(start=start(ref)[ind.tmp[1]], end=end(ref)[ind.tmp[nCG]]))		
+		})	
+		do.call(c, unname(res))
+	}, mc.cores=num.cores)
+	segments.gr <- do.call(c, unname(res))
+	
+	# Step V: find all the segments in the high-coverage regions.
+	ov <- findOverlaps(segments.gr, gals, type="within")
+	segment.read.ind <- Filter(function(x){length(x)>=min.Cover}, split(ov@subjectHits, ov@queryHits))
+
+	ov <- findOverlaps(segments.gr, ref)
+	segment.CG.ind <- split(ov@subjectHits, ov@queryHits)	
+	
+	# Step VI: calculate ME
+	mclapply(names(segment.read.ind), function(x){
+		segment.tmp <- segments.gr[as.integer(x)]
+		reads.tmp <- gals[segment.read.ind[[x]]]
+		CG.tmp <- ref[segment.CG.ind[[x]]]
+		# get meth patters
+		 mathPatterns <- lapply(1:length(reads.tmp), function(j){
+			str <- mcols(reads.tmp)$seq[[j]][start(CG.tmp) - start(reads.tmp)[j] + 1]
+			if(countPattern("C", str) + countPattern("T", str) == nCG){return(as.character(str))}
+		})			
+		freq <- table(do.call(rbind, mathPatterns))/sum(table(do.call(rbind, mathPatterns)))
+		ME <- sum(-freq * log(freq))/nCG
+		segment.tmp$ME <- ME	
+		return(segments.tmp)	
+	}, mc.cores=num.cores)
+	
+	
 	# sum(width(peaks.filter.gr))/(max(end(ref)) - min(start(ref)))
 	# filter reads not overlapped with filtered peaks
 	ov <- findOverlaps(gals, peaks.filter.gr)
